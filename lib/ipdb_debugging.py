963ca52c7bfc3f3964b07be06eb5172f7ff0bdbe
gilbert-ku <gilbert45ku@gmail.com>
2023-08-17 08:47:35 +0300

    completed assignment

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 
+# import ipdb
+
+# def plus_two(num):
+#     num + 2
+#     ipdb.set_trace()
+#     return num
 import ipdb
 
 def plus_two(num):
-    num + 2
-    return num
+    num += 2  # Add 2 to num and assign it back to num
+ 
+    return num  # Return the updated value of num
+
+result = plus_two(3)
+print(result)
